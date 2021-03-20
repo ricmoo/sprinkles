580338dc74873a222d02a6e46a831618ae88d2e3
Yuet Loo Wong <contact@yuetloo.com>
2021-03-20 18:20:11 -0400

    add asset gallery

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -2,15 +2,16 @@ let address;
 const subgraphUrl = 'https://api.thegraph.com/subgraphs/name/yuetloo/sprinkle-v0-ropsten'
 const sprinkleContractAddress = '0x2B240bdA2B9Ce6A9229E40aAbA9D1b0916D00F40';
 
+
 window.onload = () => {
    const connectButton = document.getElementById('connect');
    connectButton.addEventListener('click', handleConnect);
 
    const homePage = document.getElementById('home');
-   const dashboard = document.getElementById('dashboard');
+   const showRoom = document.getElementById('show-room');
+   const gallery = document.getElementById('gallery');
 
    const addressLabel = document.getElementById('address');
-   const dashboardContent = document.querySelector('#dashboard .card-container');
 
    const notFound = document.getElementById('not-found');
 
@@ -23,16 +24,53 @@ window.onload = () => {
       return addr.slice(0, 10) + "..." + addr.slice(-8);
    }
 
+   function showNotFound(what) {
+      const box = document.createElement('div');
+      const message = document.createTextNode(`${what} not found`);
+      box.classList.add("not-found");
+      box.appendChild(message);
+      gallery.appendChild(box);
+   }
+
+   function handleSave(token) {
+      return () => {
+
+      }
+
+   }
+
+   function showAssets(owner) {
+      console.log('sprinkle clicked')
+      return async (e) => {
+         gallery.textContent="";
+         owner = '0x23936429FC179dA0e1300644fB3B489c736D562F';
+         const assets = await fetchAssets(owner);
+         console.log('got assets', assets)
+
+         if( assets.length === 0 ) {
+            showNotFound("Assets");
+         }
+
+         assets.forEach(asset => {
+            console.log('build card asset', asset)
+            const tokenElement = buildCard(asset);
+            gallery.appendChild(tokenElement);
+            tokenElement.addEventListener('click', handleSave(asset));
+         })
+      }
+   }
+
    function gotoDashboard() {
       homePage.style.display = 'none';
-      dashboard.style.display = 'block';
+      showRoom.style.display = 'block';
       refreshDashboard();
    }
 
    function refreshDashboard() {
-      dashboardContent.textContent =""
+      gallery.textContent =""
+      const addr = '0x236ff1e97419ae93ad80cafbaa21220c5d78fb7d';
       showSpinner();
-      fetchSprinkles(address)
+      fetchSprinkles(addr)
          .then(listSprinkles)
          .finally(() => {
             hideSpinner();
@@ -53,41 +91,52 @@ window.onload = () => {
          return;
       }
 
-      notFound.style="display:none";
       sprinkles.forEach(sprinkle => {
          const token = {
             contractAddress: sprinkleContractAddress,
             id: sprinkle.id,
-            name: "Sprinkle",
+            name: `# ${ethers.BigNumber.from(sprinkle.id).toString()}`,
+            collection: "Sprinkles",
             owner: sprinkle.owner.id
          }
-         dashboardContent.appendChild(buildCard(token));
+         const tokenElement = buildCard(token);
+         gallery.appendChild(tokenElement);
+         console.log('sprinkle', sprinkle, token)
+         tokenElement.addEventListener('click', showAssets(token.owner));
       })
    }
 
    function buildCard(token) {
       const box = document.createElement("div");
-      const titleBox = document.createElement('div');
-      const idBox = document.createElement('div');
-
-      const name = document.createTextNode(token.name);
-      const idText = document.createTextNode("# " + token.id);
-
       box.classList.add("token-card");
-      titleBox.classList.add("token-name");
-      idBox.classList.add('token-id')
 
-      console.log('token', token)
-      titleBox.appendChild(name);
-      idBox.appendChild(idText);
+      if( token.imageUrl ) {
+         console.log('build image', token.imageUrl)
+         const image = document.createElement('img');
+         image.src = token.imageUrl;
+         image.width = "200";
+         image.height = "200";
+         image.alt = token.name;
+         box.appendChild(image);
+      }
 
-      box.appendChild(titleBox);
-      box.appendChild(idBox);
+      console.log('building...', token)
+      const collection = document.createElement('div');
+      const collectionText = document.createTextNode(token.collection);
+      collection.appendChild(collectionText);
+      collection.classList.add("collection");
+
+      const name = document.createElement('div');
+      const nameText = document.createTextNode(token.name);
+      name.classList.add("token-name");
+      name.appendChild(nameText)
+
+      box.appendChild(collection)
+      box.appendChild(name);
       return box;
    }
 
    async function fetchSprinkles(owner) {
-      owner = '0x236ff1e97419ae93ad80cafbaa21220c5d78fb7d'
       const json = {
          query: `
          {
@@ -96,7 +145,7 @@ window.onload = () => {
                address
                sprinkles {
                  id
-                 owner
+                 owner { id }
                  publicKeyHash
                }
             }
@@ -109,13 +158,22 @@ window.onload = () => {
       return sprinkles || [];
    }
 
-   async function fetchAssets() {
+   async function fetchAssets(owner) {
       
-      const url=`https://api.opensea.io/api/v1/assets?owner=${address}`
-      //const collectionUrl = `https://api.opensea.io/api/v1/collections?asset_owner=${address}`;
+      const url=`https://api.opensea.io/api/v1/assets?owner=${owner}`
       
       const result = await ethers.utils.fetchJson(url);
-      return result.assets;
+      const assets = result.assets.map(asset => ({
+         contractAddress: asset.asset_contract.address,
+         id: asset.token_id,
+         name: asset.name,
+         collection: asset.collection.name,
+         owner,
+         imageUrl: asset.image_url,
+         imagePreviewUrl: asset.image_preview_url,
+         imageThumbnailUrl: asset.image_thumbnail_url
+      }));
+      return assets;
    }
 
    function handleConnect() {
@@ -127,18 +185,9 @@ window.onload = () => {
       ethereum.request({ method: 'eth_requestAccounts' }).then(async accounts => {
          const provider = new ethers.providers.Web3Provider(window.ethereum);
          provider.ready.then(() => {
-            if( provider.network.chainId !== 1) {
-                  alert("Please connect to mainnet");
-                  return;
-            }
-            
             ethereum.on('accountsChanged', (newAccounts) => {
                setAccountAddress(newAccounts[0]);
                refreshDashboard();
-            });
-
-            ethereum.on('chainChanged', (chainId) => {
-               window.location.reload();
             });
 
             setAccountAddress(accounts[0]);
